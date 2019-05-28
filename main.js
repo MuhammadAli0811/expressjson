91a601dcb8cdc064bd3a28fe53d20c7eeb1f6b43
MuhammadAli0811 <m.ali0811@outlook.com>
2019-05-28 11:54:06 +0500

    Updated Work

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 // console.log(received.send)
 
+const datafile = require("./data/data.json");
+
 const express = require("express");
 const app = express();
 //console.log(app);
@@ -27,4 +29,31 @@ app.get("/user/:id",(req,res) => {
     res.send("hi users"+req.params.id);
 })
 
+
+
+app.get("/friend",(req,res)=> {
+    var info ='';
+   datafile.friend.forEach(function(item) {
+       info +=`
+       <li>
+       <h1>${item.Title}</h1>
+       <p>${item.Name}</p>
+       </li>
+       `;
+   });
+    res.send(`${info}`)
+})
+
+
+
+
+
+app.get("/friend/:id",(req,res)=> {
+  var data = datafile.friend[req.params.id];
+
+    res.send(`
+                <h3> ${data.Title}  </h3>
+                <p> ${data.Name}   </p>
+                 `)
+})
 app.listen(3000,() => console.log("Go at http://localhost:3000") )
